48de823a73eedebf9de62867bcde4638eed3fa1b
Devin Chen <d3vin.chen@gmail.com>
2017-06-03 23:36:16 +0800

    switch lang with dropdown menu

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,24 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { FormattedMessage } from 'react-intl';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import fr from 'react-intl/locale-data/fr';
+import zh from 'react-intl/locale-data/zh';
+
+addLocaleData([...en, ...fr, ...zh]);
+const messages = {
+  zh: {
+    welcome: '你好',
+    join: 'By joining, I agree to Mozilla Webmaker’s {code}',
+  }
+};
 
 const name = 'Eric';
 const unreadCount = 1000;
 
-const Root = () => (
-  <div>
-    <p>Hello React Hot Loader!</p>
-    <FormattedMessage
-        id="welcome"
-        defaultMessage={`Hello {name}, you have {unreadCount, number} {unreadCount, plural,
-          one {message}
-          other {messages}
-        }`}
-        values={{
-          name: <code>{name}</code>,
-          unreadCount
-        }}
-    />
-  </div>
-)
+class Root extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'en',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+  render() {
+    const { value } = this.state;
+    return (
+      <div>
+        <p>Hello React Hot Loader!</p>
+        <select name="" id="" onChange={this.handleChange}>
+          <option value="en">en</option>
+          <option value="zh">zh</option>
+        </select>
+        <IntlProvider
+          locale={value}
+          messages={messages[value]}
+        >
+          <FormattedMessage
+              id="welcome"
+              defaultMessage={`Hello {name}, you have {unreadCount, number} {unreadCount, plural,
+                one {message}
+                other {messages}
+              }`}
+              values={{
+                name: <code>{name}</code>,
+                unreadCount
+              }}
+          />
+        </IntlProvider>
+      </div>
+    );
+  }
+}
 
-export default Root
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
-import { IntlProvider, addLocaleData } from 'react-intl';
-import en from 'react-intl/locale-data/en';
-import fr from 'react-intl/locale-data/fr';
-import zh from 'react-intl/locale-data/zh';
 
 import Root from './containers/Root'
 
-addLocaleData([...en, ...fr, ...zh]);
-const messages = {
-  welcome: '你好',
-  join: 'By joining, I agree to Mozilla Webmaker’s {code}',
-}
-
 const render = Component => {
   ReactDOM.render(
-    <IntlProvider locale="zh" messages={messages}>
-      <AppContainer>
-        <Component />
-      </AppContainer>
-    </IntlProvider>,
+    <AppContainer>
+      <Component />
+    </AppContainer>,
     document.getElementById('root')
   )
 }
