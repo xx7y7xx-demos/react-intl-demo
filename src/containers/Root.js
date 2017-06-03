e8b0075d7e1e10d3458aef5572e2723b2f821252
Devin Chen <d3vin.chen@gmail.com>
2017-06-03 18:50:40 +0800

    simple react-intl example

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl';
+
+const name = 'Eric';
+const unreadCount = 1000;
 
 const Root = () => (
-  <div>Hello React Hot Loader!</div>
+  <div>
+    <p>Hello React Hot Loader!</p>
+    <FormattedMessage
+        id="welcome"
+        defaultMessage={`Hello {name}, you have {unreadCount, number} {unreadCount, plural,
+          one {message}
+          other {messages}
+        }`}
+        values={{name: <b>{name}</b>, unreadCount}}
+    />
+  </div>
 )
 
 export default Root
